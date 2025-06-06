f077cc9b993b58e73520e11ce1de7a3871e7f627
Poplo C <CPoplo@outlook.com>
2025-06-06 19:51:16 +0800

    update 0.0.1

--- Properties/Resources.en.Designer.cs
+++ Properties/Resources.en.Designer.cs
@@ -22,14 +22,14 @@ namespace unreal_GUI.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
+    public class Resources___复制 {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal Resources___复制() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace unreal_GUI.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("unreal_GUI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("unreal_GUI.Properties.Resources - 复制", typeof(Resources___复制).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
