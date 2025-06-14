7ef7a8c878542568f73fe4d938c2ebcdbf2dde0e
Poplo C <CPoplo@outlook.com>
2025-06-14 20:14:19 +0800

    111111

--- About.xaml.cs
+++ About.xaml.cs
@@ -1,53 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using unreal_GUI.ViewModel;
 
 namespace unreal_GUI
 {
     /// <summary>
     /// About.xaml 的交互逻辑
     /// </summary>
-    public partial class About : System.Windows.Controls.UserControl
+    public partial class About : UserControl
     {
-
-        private string LatestVersion;
-        private string LastUpdateTime;
-        
-
-
         public About()
         {
             InitializeComponent();
-            // 控件信息更新
-            LastUpdateTime = Properties.Settings.Default.LastUpdateTime.ToString();
-            LatestVersion = Properties.Settings.Default.LatestVersion.ToString();
-            Version.Text = "当前版本：" + Application.ResourceAssembly.GetName().Version.ToString();
-            DataContext = this;
-            Tip.Text = $"最新可用版本：{LatestVersion}";
-            Update_Time.Text = $"上次更新时间：{LastUpdateTime}";
+            DataContext = new AboutViewModel();
         }
-        
-
-               
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo { FileName = "https://github.com/G-POPLO/unreal-GUI/releases/", UseShellExecute = true });
-        }
-
-        
     }
 }
 
--- Rename.xaml.cs
+++ Rename.xaml.cs
@@ -1,109 +1,18 @@
-using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-
+using unreal_GUI.ViewModel;
 
 namespace unreal_GUI
 {
     /// <summary>
     /// Rename.xaml 的交互逻辑
     /// </summary>
-    public partial class Rename : System.Windows.Controls.UserControl
+    public partial class Rename : UserControl
     {
         public Rename()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-            // 创建FolderBrowserDialog实例
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-           
-
-            // 显示对话框并检查用户是否选择了文件夹
-            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
-            {
-                // 获取选择的文件夹路径
-                string selectedPath = folderBrowserDialog.SelectedPath;
-                Input.Text = selectedPath;
-            }
-        }
-
-        private void RenameButton_Click(object sender, RoutedEventArgs e)
-        {
-            string projectPath = Input.Text;
-            string newName = Output.Text;
-            string command;
-
-            if (rbProject.IsChecked == true)
-            {
-                command = $"renom rename-project --project {projectPath} --new-name {newName}";
-            }
-            else
-            {
-                command = $"renom rename-plugin --project {projectPath} --plugin {projectPath} --new-name {newName}";
-            }
-
-            try
-            {
-                string exePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App", "renom.exe");
-                var processInfo = new ProcessStartInfo
-                {
-                    FileName = exePath,
-                    Arguments = command.Replace("renom ", ""),
-                    UseShellExecute = true,
-                    CreateNoWindow = true
-                };
-                System.Diagnostics.Process.Start(processInfo);
-
-
-                
-                msg.Text = "重命名成功！";
-                msg.Visibility = Visibility.Visible;
-
-
-                // 检查AutoOpen设置来决定是否打开文件夹
-                if (Properties.Settings.Default.AutoOpen)
-                {
-                    // 更新输入框为新的路径
-                    string newPath = Path.Combine(System.IO.Path.GetDirectoryName(projectPath), newName);
-                    Input.Text = newPath;
-                    msg.Text = "重命名成功！";
-                    var player = new System.Media.SoundPlayer(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sound", "ui-sound-on.wav"));
-                    player.Play();
-                    msg.Visibility = Visibility.Visible;
-                    Process.Start(new ProcessStartInfo
-                {
-                    FileName = "explorer.exe",
-                    Arguments = newPath,
-                    UseShellExecute = true
-                });
-                }
-            }
-            catch (Exception ex)
-            {
-                msg.Text = $"重命名失败：{ex.Message}";
-                msg.Visibility = Visibility.Visible;               
-                var player = new System.Media.SoundPlayer(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sound", "ui-sound-off.wav"));
-                player.Play();
-            }
+            DataContext = new RenameViewModel();
         }
     }
 }
+
