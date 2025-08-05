b32bc0a7a8568ce676e2b202bc1d0b403ba81333
Poplo C <CPoplo@outlook.com>
2025-08-05 10:43:27 +0800

    Update 0.3.0.0

--- Model/ModernDialog.cs
+++ Model/ModernDialog.cs
@@ -1,6 +1,7 @@
 using ModernWpf;
 using ModernWpf.Controls;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -77,14 +78,13 @@ namespace unreal_GUI.Model
                 PrimaryButtonText = "添加",
                 CloseButtonText = "取消",
                 Content = content,
-                
-                
+                DefaultButton = ContentDialogButton.Primary,
                 IsPrimaryButtonEnabled = false // 初始禁用添加按钮
             };
-            
+
             // 设置对话框引用
             content.Dialog = dialog;
-            
+
             var result = await dialog.ShowAsync();
             if (result == ContentDialogResult.Primary)
             {
@@ -99,25 +99,30 @@ namespace unreal_GUI.Model
                         var json = File.ReadAllText("settings.json");
                         settings = JsonConvert.DeserializeObject<SettingsViewModel.SettingsData>(json) ?? new SettingsViewModel.SettingsData();
                     }
-                    
-                    // 添加新的自定义按钮信息
+
+                    // 若不存在配置，则初始化列表
                     if (settings.CustomButtons == null)
                     {
                         settings.CustomButtons = [];
                     }
                     settings.CustomButtons.Add(new SettingsViewModel.CustomButton { Name = buttonName, Path = folderPath });
-                    
                     // 保存更新后的设置
                     var jsonSettings = JsonConvert.SerializeObject(settings, Formatting.Indented);
                     File.WriteAllText("settings.json", jsonSettings);
-
-                    // 这段刷新UI的代码被注释掉了，因为并没有作用
-                    //var viewModel = Application.Current.MainWindow?.DataContext as QuickAccessViewModel;
-                    //viewModel?.LoadCustomButtons();
                 }
+
+
+            }
+            if (result == ContentDialogResult.Primary)
+            {
+                // 通知MainWindow页面刷新
+                var mainWindowViewModel = Application.Current.MainWindow.DataContext as MainWindowViewModel;
+                mainWindowViewModel?.NavigateToQuickAccessCommand.Execute(null);
             }
             return result;
         }
+
+
 
         /// <summary>
         /// 显示"删除自定义按钮"的对话框
@@ -125,17 +130,18 @@ namespace unreal_GUI.Model
         public static async Task<ContentDialogResult> ShowDeleteCustomDialogAsync()
         {
             var content = new Delete_DialogContent();
-            
+
             // 读取现有设置并更新 ListBox
             if (File.Exists("settings.json"))
             {
                 var json = File.ReadAllText("settings.json");
                 var settings = JsonConvert.DeserializeObject<SettingsViewModel.SettingsData>(json);
-                
+
                 if (settings?.CustomButtons != null)
                 {
                     content.ButtonsListBox.ItemsSource = settings.CustomButtons;
                 }
+
             }
 
             ContentDialog dialog = new ContentDialog
@@ -146,17 +152,17 @@ namespace unreal_GUI.Model
                 Content = content
             };
             var result = await dialog.ShowAsync();
-            // 这段刷新UI的代码被注释掉了，因为并没有作用
-            //var viewModel = Application.Current.MainWindow?.DataContext as QuickAccessViewModel;
-            //viewModel?.LoadCustomButtons();
-            //if (result == ContentDialogResult.Primary)
-            //{
-            //}
-            //else
-            //{
-            //    
-            //}
+
+
+            if (result == ContentDialogResult.Primary)
+            {
+                // 通知MainWindow页面刷新
+                var mainWindowViewModel = Application.Current.MainWindow.DataContext as MainWindowViewModel;
+                mainWindowViewModel?.NavigateToQuickAccessCommand.Execute(null);
+            }
+
             return result;
+
         }
     }
 }
